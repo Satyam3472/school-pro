28e080be7144beed32159df1efee37f2885706eb
Geetanjali Kashyap <geetanjalikashyap@Geetanjalis-MacBook-Air.local>
2025-07-13 23:45:38 +0530

    Tunnel created

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+    content: [
+      "./pages/**/*.{js,ts,jsx,tsx}",    // for pages directory
+      "./components/**/*.{js,ts,jsx,tsx}", // for components
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  

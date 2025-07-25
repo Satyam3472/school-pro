37ffc6afa12c102a94d75c8190192959d435a493
Geetanjali Kashyap <geetanjalikashyap@Geetanjalis-MacBook-Air.local>
2025-07-25 07:59:21 +0530

    docker-compose issue

--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -1,18 +1,27 @@
+import { Settings } from "lucide-react";
 import React from "react";
+import { Button } from "./ui/button";
 
 export function NavUser({ user }: { user: { name: string; email: string; avatar: string } }) {
   return (
-    <div className="flex items-center gap-3 p-2">
-      <img
-        src={user.avatar}
-        alt={user.name}
-        className="w-10 h-10 rounded-full object-cover border"
-        onError={(e) => (e.currentTarget.src = "/assets/school_logo.png")}
-      />
-      <div>
-        <div className="font-semibold leading-tight">{user.name}</div>
-        <div className="text-xs text-muted-foreground">{user.email}</div>
+    <>
+
+      <Button variant="outline" size="sm" className="gap-1.5">
+            <Settings className="w-3.5 h-3.5" />
+            <span>Settings</span>
+      </Button>
+      <div className="flex items-center gap-3 p-2">
+        <img
+          src={user.avatar}
+          alt={user.name}
+          className="w-10 h-10 rounded-full object-cover border"
+          onError={(e) => (e.currentTarget.src = "/assets/school_logo.png")}
+        />
+        <div>
+          <div className="font-semibold leading-tight">sdfdss{user.name}</div>
+          <div className="text-xs text-muted-foreground">{user.email}</div>
+        </div>
       </div>
-    </div>
+    </>
   );
 } 

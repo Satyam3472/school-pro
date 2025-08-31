ef912fdd73e282b4d4ec4dbba47ca18850ec8f06
Satyam3472 <kumarsatyam8298380149@gmail.com>
2025-08-31 18:59:16 +0530

    bug fixing v-1

--- init.sql
+++ init.sql
@@ -1,13 +1,12 @@
--- init.sql
+
+CREATE DATABASE IF NOT EXISTS school_db;
 -- Create the application user
 CREATE USER 'AdminUser'@'%' IDENTIFIED BY 'AdminUser';
 
 -- Grant privileges to the application user on your application database
 GRANT ALL PRIVILEGES ON school_db.* TO 'AdminUser'@'%';
 
--- Grant necessary system privileges for Prisma
-GRANT CREATE, ALTER, DROP, REFERENCES, INDEX, SELECT, INSERT, UPDATE, DELETE ON `school_db`.* TO `AdminUser`@`%`;
-GRANT SELECT ON `mysql`.* TO `AdminUser`@`%`;
+-- Prisma needs read-only on information_schema & performance_schema
 GRANT SELECT ON `performance_schema`.* TO `AdminUser`@`%`;
 GRANT SELECT ON `information_schema`.* TO `AdminUser`@`%`;
 
